0a2fa24a4e89d90c3330bc0218e176d6b439fd1b
pedro <p007bon@gmail.com>
2024-02-01 16:47:46 -0600

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
 import Router from 'svelte-spa-router'
 import routes from './routes';
+
+import Menu from './Componentes/Menu.svelte';
 </script>
 
 <main>
+<Menu />
 <Router {routes} />
 </main>
 
---
+++ src/Componentes/Menu.svelte
@@ -0,0 +1,46 @@
+<script>
+
+</script>
+
+<main>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <img src="img/logo-la-clinica-dental-oscuro.png" alt="">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="!#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="!#">Link</a>
+              </li>
+              
+            </ul>
+            <button class="btn agenda">
+                <i class="bi bi-whatsapp"></i>
+                Agenda tu Primera Cita Gratis
+            </button>
+            <button class="btn llamada">
+                <i class="bi bi-telephone"></i>
+                Llámanos
+            </button>
+          </div>
+        </div>
+      </nav>
+</main>
+
+<style>
+    /* COLOR QUE SE USA EN lCD #5EBCD6 */
+    .agenda{
+        background-color:#5EBCD6;
+        color: white;
+        margin-right: 10px;
+    }
+    .llamada{
+        background-color:#5EBCD6; 
+        color: white;
+    }
+</style>
