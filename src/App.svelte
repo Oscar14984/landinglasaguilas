fe2e549fbcfa92e49af45c9e9999b285b6136954
pedro <p007bon@gmail.com>
2024-02-02 10:33:40 -0600

    update footer

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,13 @@ import Router from 'svelte-spa-router'
 import routes from './routes';
 
 import Menu from './Componentes/Menu.svelte';
+import Footer from './Componentes/Footer.svelte';
 </script>
 
 <main>
 <Menu />
 <Router {routes} />
+<Footer />
 </main>
 
 <style>
---
+++ src/Componentes/Footer.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<main>
+    <footer class="gridContainer">
+        <img src="img/logodental-Light.png" alt="">
+        <!-- Social Media -->
+
+    </footer>
+</main>
+
+<style>
+    /* COLOR QUE SE USARA EN EL FOOTER #2D3549 */
+    /* COLOR QUE SE USARA EN LA PARTE DE DERECHOS RESERVADOS #192135 */
+    footer{
+        background-color: #2D3549;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        color: white;
+        /* text-align: center; */
+    }
+</style>
