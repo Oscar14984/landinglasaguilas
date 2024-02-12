1b365423079a4a1576115317a938e062ee961f6e
pedro <p007bon@gmail.com>
2024-02-12 17:51:33 -0600

    update

--- src/Principal/UsuarioFinal/Contactanos.svelte
+++ src/Principal/UsuarioFinal/Contactanos.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <main>
-
+    <div class="contacto-principal">
+        <h1>Contactanos</h1>
+        <div class="contenido-p-contacto-principal">
+            <p>Llámanos 5589003683 y consigue ¡la sonrisa de tus sueños!</p>
+        </div>
+    </div>
 </main>
 
 <style>
